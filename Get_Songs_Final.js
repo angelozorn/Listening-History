c703542dfadf0f3d1c8bf0bb3c40d952e90c6a1b
angelozorn <angelozorn2017@gmail.com>
2024-02-15 19:10:54 -0800

    first commit

---
+++ Get_Songs_Final.js
@@ -0,0 +1,251 @@
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const btoa = require('btoa');
+const process = require('process');
+const Promise = require('bluebird');
+
+var mysql = require('mysql');
+const util = require('util');
+
+const credentials = JSON.parse(fs.readFileSync(__dirname + '/credentials.json'));
+const redirect_uri = 'http://localhost:8888/callback';
+
+
+
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "WearyBunny12916",
+  database: "spotify_listening_history"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+const readFileAsync = util.promisify(fs.readFile);
+const queryAsync = util.promisify(con.query.bind(con));
+
+if (fs.existsSync(__dirname + '/authorization.json')) {
+  var authorization = JSON.parse(fs.readFileSync(__dirname + '/authorization.json'));
+  saveRecentTracks(authorization);
+} else {
+  authorizeWithCode(
+    credentials.client_id,
+    credentials.client_secret,
+    credentials.authorization_code,
+    redirect_uri
+  ).then(function(tokens) {
+    fs.writeFileSync(
+      __dirname + '/authorization.json',
+      JSON.stringify(tokens, null, 2)
+    );
+    saveRecentTracks(tokens);
+  }).catch(function(error) {
+    console.error(error);
+    process.exit(0);
+  });
+}
+
+
+async function saveRecentTracks(authorization) {
+    try {
+      const newAuthorization = await authorizeWithRefreshToken(
+        credentials.client_id,
+        credentials.client_secret,
+        authorization.refresh_token
+      );
+  
+      var latestTimestamp;
+      var directory = __dirname + '/data/';
+  
+      try {
+        const track = await readFirstTrackFrom(directory + "latest.json");
+        latestTimestamp = track.played_at;
+      } catch (error) {
+        console.error(error);
+        // Handle error if needed
+        return process.exit(1);
+      }
+  
+      const tracks = await getRecentTracks(newAuthorization.access_token, latestTimestamp);
+  
+      if (tracks.items.length === 0) {
+        console.log('No new songs have played since ' + new Date(latestTimestamp).toLocaleString());
+      } else {
+        // Insert tracks directly into MySQL tables
+        const mostRecentTrack = tracks.items[0];
+        fs.writeFileSync(directory + "latest.json", JSON.stringify(mostRecentTrack, null, 2));
+
+        for (const track of tracks.items) {
+          const simplifiedTrack = {
+            artist: track.track.artists.map(artist => artist.name).join(', '),
+            songName: track.track.name,
+            trackId: track.track.id,
+            playTime: track.played_at,
+            duration: track.track.duration_ms,
+            albumName: track.track.album.name
+          };
+  
+          try {
+            // Insert into MySQL 'songs' table
+            await queryAsync(
+              'INSERT INTO songs (artist, songName, trackId, playTime, duration, albumName) VALUES (?, ?, ?, ?, ?, ?)',
+              Object.values(simplifiedTrack)
+            );
+  
+            // Split the artists by comma and insert into 'split_songs' table
+            for (const artist of simplifiedTrack.artist.split(',')) {
+              const artist_track = {
+                artist: artist.trim(),
+                song: simplifiedTrack.songName,
+                trackId: simplifiedTrack.trackId,
+                playTime: simplifiedTrack.playTime,
+                duration: simplifiedTrack.duration,
+                albumName: simplifiedTrack.albumName
+              };
+  
+              await queryAsync(
+                `INSERT INTO split_songs (artist, songName, trackId, playTime, duration, albumName) VALUES (?, ?, ?, ?, ?, ?)`,
+                Object.values(artist_track)
+              );
+            }
+  
+            console.log('Inserted songs into MySQL tables.');
+          } catch (error) {
+            console.error(error);
+            // Handle error if needed
+            return process.exit(1);
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle error if needed
+      return process.exit(1);
+    } finally {
+      process.exit(0);
+    }
+  }
+  
+
+  function readFirstTrackFrom(file) {
+    return new Promise(function(resolve, reject) {
+      try {
+        var text = fs.readFileSync(file, 'utf8');
+        var content = JSON.parse(text);
+  
+        if (content && content.played_at && content.track) {
+          resolve({
+            played_at: content.played_at,
+            track: {
+              artist: content.track.artists.map(artist => artist.name).join(', '),
+              songName: content.track.name,
+              trackId: content.track.id,
+              playTime: content.played_at,
+              duration: content.track.duration_ms,
+              albumName: content.track.album.name
+            }
+          });
+        } else {
+          // If the expected properties are not present, resolve with a default value
+          reject(new Error('Invalid structure in latest.json'));
+        }
+      } catch (error) {
+        // If an error occurs during file read or JSON parse, reject with the error
+        reject(error);
+      }
+    });
+  }
+  
+  
+  function authorizeWithRefreshToken(client_id, client_secret, refresh_token) {
+    var botaAuth = btoa(client_id + ":" + client_secret);
+    return new Promise(function(resolve, reject) {
+      request({
+        url: "https://accounts.spotify.com/api/token",
+        method: 'POST',
+        form: {
+          grant_type: 'refresh_token',
+          refresh_token: refresh_token
+        },
+        headers: {
+          'Authorization': "Basic " + botaAuth
+        }
+      }, function(e, response) {
+        if (e) {
+          reject(e);
+          return;
+        }
+        resolve(JSON.parse(response.body));
+      });
+    });
+  }
+  
+  function authorizeWithCode(client_id, client_secret, authorization_code, redirect_uri) {
+    var botaAuth = btoa(client_id + ":" + client_secret);
+    return new Promise(function(resolve, reject) {
+      request({
+        url: "https://accounts.spotify.com/api/token",
+        method: 'POST',
+        form: {
+          grant_type: 'authorization_code',
+          code: authorization_code,
+          redirect_uri: redirect_uri
+        },
+        headers: {
+          'Authorization': "Basic " + botaAuth
+        }
+      }, function(e, response) {
+        if (e) {
+          reject(e);
+          return;
+        }
+        const body = JSON.parse(response.body);
+        if (body.access_token) {
+          resolve(body);
+        } else {
+          const err = new Error('Response does not have access_token');
+          err.body = body;
+          reject(err);
+        }
+      });
+    });
+  }
+  
+  function getRecentTracks(access_token, afterTimestamp) {
+    return new Promise(function(resolve, reject) {
+      console.log("Getting tracks after " + new Date(afterTimestamp).toLocaleString());
+      const q = afterTimestamp ? {
+        after: (new Date(afterTimestamp)).getTime(),
+        limit: 50
+      } : {
+        limit: 50
+      };
+      request({
+        url: "https://api.spotify.com/v1/me/player/recently-played",
+        method: 'GET',
+        qs: q,
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        }
+      }, function(e, response) {
+        if (e) {
+          reject(e);
+          return;
+        }
+        resolve(JSON.parse(response.body));
+      });
+    });
+  }
+  
+  
+  // Close connections after processing
+  process.on('exit', () => {
+   con.end();
+  });
+  
---
+++ dash_dashboard.py
@@ -0,0 +1,296 @@
+import dash
+from dash import dash_table
+from dash import html, dcc, callback, Output, Input, State
+import dash_bootstrap_components as dbc
+import mysql.connector
+import pandas as pd
+
+# Replace these values with your actual database credentials
+db_config = {
+    'host': "localhost",
+    'user': "root",
+    'password': "WearyBunny12916",
+    'database': "spotify_listening_history"
+}
+
+# Establish a connection to the database
+connection = mysql.connector.connect(**db_config)
+
+try:
+    if connection.is_connected():
+        print("Connected to the MySQL database")
+
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Query for the 'songs' table
+        songs_query = """ SELECT DISTINCT
+                            Artist,
+                            songName,
+                            DATE_FORMAT(playTime, '%m/%d/%Y') AS playDate,
+                            COUNT(Duration) AS listenCount,
+                            SUM(Duration) AS timeListened
+                        FROM
+                            songs
+                        GROUP BY
+                            Artist, songName, playDate;"""
+
+        # Execute the 'songs' query
+        cursor.execute(songs_query)
+
+        # Fetch all the results for 'songs'
+        songs_result = cursor.fetchall()
+
+        # Get column names for 'songs'
+        songs_columns = [desc[0] for desc in cursor.description]
+
+        # Create a Pandas DataFrame for 'songs'
+        songs_df = pd.DataFrame(songs_result, columns=songs_columns)
+
+        # Group by "Artist" and "songName" and sum the columns for initialization
+        initial_grouped_songs_df = songs_df.groupby(['Artist', 'songName']).agg({
+            'listenCount': 'sum',
+            'timeListened': 'sum'
+        }).reset_index()
+
+        # Sort by listenCount in descending order and take the top 10
+        initial_top_songs_df = initial_grouped_songs_df.sort_values(by='listenCount', ascending=False).head(10)
+
+        # Add a rank column
+        initial_top_songs_df['Rank'] = initial_top_songs_df['listenCount'].rank(ascending=False, method='first')
+
+        # Convert the dataframe to dictionary for DataTable
+        initial_songs_data = initial_top_songs_df.copy()
+        initial_songs_data['timeListened'] = (initial_songs_data['timeListened'] / 60000).round().astype(int)
+        initial_songs_data = initial_songs_data.to_dict('records')
+
+        # Query for the new analytics query
+        analytics_query = """
+            SELECT DISTINCT
+                Artist,
+                DATE_FORMAT(playTime, '%m/%d/%Y') AS playDate,
+                COUNT(Duration) AS listenCount,
+                SUM(Duration) AS timeListened
+            FROM
+                split_songs
+            
+            GROUP BY
+                Artist, playDate;
+        """
+
+        # Execute the analytics query
+        cursor.execute(analytics_query)
+
+        # Fetch all the results for analytics
+        analytics_result = cursor.fetchall()
+
+        # Get column names for analytics
+        analytics_columns = [desc[0] for desc in cursor.description]
+
+        # Create a Pandas DataFrame for analytics
+        analytics_df = pd.DataFrame(analytics_result, columns=analytics_columns)
+
+        # Group by "Artist" and sum the columns for initialization
+        initial_grouped_artists_df = analytics_df.groupby(['Artist']).agg({
+            'listenCount': 'sum',
+            'timeListened': 'sum'
+        }).reset_index()
+
+        # Sort by listenCount in descending order and take the top 10
+        initial_top_artists_df = initial_grouped_artists_df.sort_values(by='listenCount', ascending=False).head(10)
+
+        # Add a rank column
+        initial_top_artists_df['Rank'] = initial_top_artists_df['listenCount'].rank(ascending=False, method='first')
+
+        # Convert the dataframe to dictionary for DataTable
+        initial_artists_data = initial_top_artists_df.copy()
+        initial_artists_data['timeListened'] = (initial_artists_data['timeListened'] / 60000).round().astype(int)
+        initial_artists_data = initial_artists_data.to_dict('records')
+
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    # Close the cursor and connection in the finally block
+    if 'cursor' in locals() and cursor is not None:
+        cursor.close()
+
+    if connection.is_connected():
+        connection.close()
+        print("Connection closed")
+
+# Initialize the Dash app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Define the app layout
+app.layout = dbc.Container(
+    [
+        dcc.DatePickerRange(
+            id='date-range-picker',
+            start_date=songs_df['playDate'].min(),
+            end_date=songs_df['playDate'].max(),
+            display_format='MM/DD/YYYY',
+            style={
+                'font-family': 'Arial, sans-serif',
+                'margin-bottom': '20px',
+                'background-color': 'black',
+                'color': 'white !important'
+                }
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H3("Most Played Songs", style={'color': '#1DB954'}),
+                    width=12,  # Full width for the header
+                )
+            ],
+            style={'backgroundColor': 'black', 'margin-bottom': '10px'}  # Set the background color for the row
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dash_table.DataTable(
+                        id='songs-table',
+                        columns=[
+                            {'name': 'Rank', 'id': 'Rank'},
+                            {'name': 'Artist', 'id': 'Artist'},
+                            {'name': 'Song', 'id': 'songName'},  # Update column header
+                            {'name': 'Plays', 'id': 'listenCount'},  # Update column header
+                            {'name': 'Time Listened', 'id': 'timeListened'},  # Update column header
+                        ],
+                        data=initial_songs_data,  # Initial data for the table
+                        style_table={'backgroundColor': 'black'},
+                        style_header={'backgroundColor': 'black', 'color': 'white'},
+                        style_cell={
+                            'backgroundColor': 'black',
+                            'color': 'white',
+                            'textAlign': 'center',  # Center text horizontally
+                            'font-family': 'Arial, sans-serif'
+                        },
+                    ),
+                    width=12,  # Full width for the table
+                ),
+            ],
+            style={'backgroundColor': 'black', 'margin-bottom': '20px'}  # Set the background color for the row
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H3("Most Played Artists", style={'color': '#1DB954'}),
+                    width=12,  # Full width for the header
+                )
+            ],
+            style={'backgroundColor': 'black', 'margin-bottom': '10px'}  # Set the background color for the row
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dash_table.DataTable(
+                        id='artists-table',
+                        columns=[
+                            {'name': 'Rank', 'id': 'Rank'},
+                            {'name': 'Artist', 'id': 'Artist'},
+                            {'name': 'Plays', 'id': 'listenCount'},  # Update column header
+                            {'name': 'Time Listened', 'id': 'timeListened'},  # Update column header
+                        ],
+                        data=initial_artists_data,  # Initial data for the table
+                        style_table={'backgroundColor': 'black', 'margin-bottom': '200px'},
+                        style_header={'backgroundColor': 'black', 'color': 'white'},
+                        style_cell={
+                            'backgroundColor': 'black',
+                            'color': 'white',
+                            'textAlign': 'center',  # Center text horizontally
+                            'font-family': 'Arial, sans-serif'
+                        },
+                    ),
+                    width=12,  # Full width for the table
+                ),
+            ],
+            style={'backgroundColor': 'black', 'margin-bottom': '200px'}  # Set the background color for the row
+        ),
+    ],
+    fluid=True,
+    style={'backgroundColor': 'black', 'margin-bottom': '200px'},  # Set the background color of the container
+)
+
+# Define callback to update songs table based on date range
+@app.callback(
+    Output('songs-table', 'data'),
+    [Input('date-range-picker', 'start_date'), Input('date-range-picker', 'end_date')],
+    prevent_initial_call=True,
+)
+def update_songs_table(start_date, end_date):
+    # Convert date strings to datetime objects
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    # Convert playDate column to datetime objects in songs_df
+    songs_df['playDate'] = pd.to_datetime(songs_df['playDate'])
+
+    # Filter songs data based on date range
+    filtered_songs_df = songs_df[
+        (songs_df['playDate'] >= start_date) & (songs_df['playDate'] <= end_date)
+    ]
+
+    # Group by "Artist" and "songName" and sum the columns
+    grouped_songs_df = filtered_songs_df.groupby(['Artist', 'songName']).agg({
+        'listenCount': 'sum',
+        'timeListened': 'sum'
+    }).reset_index()
+
+    # Sort by listenCount in descending order and take the top 10
+    top_songs_df = grouped_songs_df.sort_values(by='listenCount', ascending=False).head(10)
+
+    # Add a rank column
+    top_songs_df['Rank'] = top_songs_df['listenCount'].rank(ascending=False, method='first')
+
+    # Convert the timeListened column to minutes and round to the nearest whole number
+    top_songs_df['timeListened'] = (top_songs_df['timeListened'] / 60000).round().astype(int)
+
+    # Convert the dataframe to dictionary for DataTable
+    top_songs_data = top_songs_df.to_dict('records')
+
+    return top_songs_data
+
+# Define callback to update artists table based on date range
+@app.callback(
+    Output('artists-table', 'data'),
+    [Input('date-range-picker', 'start_date'), Input('date-range-picker', 'end_date')],
+    prevent_initial_call=True,
+)
+def update_artists_table(start_date, end_date):
+    # Convert date strings to datetime objects
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    # Convert playDate column to datetime objects in analytics_df
+    analytics_df['playDate'] = pd.to_datetime(analytics_df['playDate'])
+
+    # Filter artists data based on date range
+    filtered_artists_df = analytics_df[
+        (analytics_df['playDate'] >= start_date) & (analytics_df['playDate'] <= end_date)
+    ]
+
+    # Group by "Artist" and sum the columns
+    grouped_artists_df = filtered_artists_df.groupby(['Artist']).agg({
+        'listenCount': 'sum',
+        'timeListened': 'sum'
+    }).reset_index()
+
+    # Sort by listenCount in descending order and take the top 10
+    top_artists_df = grouped_artists_df.sort_values(by='listenCount', ascending=False).head(10)
+
+    # Add a rank column
+    top_artists_df['Rank'] = top_artists_df['listenCount'].rank(ascending=False, method='first')
+
+    # Convert the timeListened column to minutes and round to the nearest whole number
+    top_artists_df['timeListened'] = (top_artists_df['timeListened'] / 60000).round().astype(int)
+
+    # Convert the dataframe to dictionary for DataTable
+    top_artists_data = top_artists_df.to_dict('records')
+
+    return top_artists_data
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
